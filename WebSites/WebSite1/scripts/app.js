762f366610665d6261b954098d944a2a28be4f77
aalshawa97 <abdullahalshawa@gmail.com>
2018-02-17 13:21:27 -0800

    Extra Credit For Registration Form Validation

--- WebSites/WebSite1/scripts/app.js
+++ WebSites/WebSite1/scripts/app.js
@@ -1,10 +1,23 @@
 ﻿function displayInfo() {
 
-    alert("displaying");
-
     var firstName = document.getElementById("firstName").value;
+    if (firstName == "")
+    {
+        alert("First name must be entered");
+        return false;
+    }
+
     var lastName = document.getElementById("lastName").value;
+    if (lastName == "") {
+        alert("Last name must be entered");
+        return false;
+    }
+
     var age = document.getElementById("age").value;
+    if (age == "") {
+        alert("Last name must be entered");
+        return false;
+    }
 
     var summary = firstName + " " + lastName + " is " + age + " years old.";
 
